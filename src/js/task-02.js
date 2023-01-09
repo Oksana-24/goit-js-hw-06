59dbf7ecefbc8d2858cf6348b8f92d32f9470b09
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-01-09 20:38:33 +0100

    add task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,10 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = document.querySelector('#ingredients');
+console.dir(list);
+let markup = '';
+for (let i = 0; i< ingredients.length; i+=1) {
+  markup += `<li class="item">${ingredients[i]}</li>`;
+}
+list.insertAdjacentHTML('beforeend', markup);
