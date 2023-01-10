da73cce2940097fa33428111900c71d1c127c2c0
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-01-10 12:24:47 +0100

    add task9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return   `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const body = document.body;
+const button = document.querySelector('.change-color');
+const outputColor = document.querySelector('.color');
+
+button.addEventListener('click', onButton); 
+
+function onButton(event) {
+  let newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  outputColor.textContent = newColor;
+} 
