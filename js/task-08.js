a200268224984f7b7a28c79fa3fb008a7f9af395
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-01-18 11:04:08 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -5,10 +5,20 @@ function handleSubmit(event) {
     event.preventDefault();
     const {
         elements: { email, password } } = event.currentTarget;
+    
+        const userEmail = email.value;
+        const userPassword = password.value;
+
+
     if (email.value === "" || password.value === "") {
         return alert("Please, fill in all inputs");
     }
-    console.dir(`Email: ${email.value} \n Password: ${password.value}`);
+    // console.dir(`Email: ${email.value} \n Password: ${password.value}`);
+        const formData2 = {
+        userEmail,
+        userPassword,
+    };
+    console.log(formData2);
     event.currentTarget.reset();
 
 }
